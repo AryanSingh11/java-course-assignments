2eb3dd21f75cf1e4150ee798af8affcd9b329f19
Aryan Singh <aryansingh22070@gmail.com>
2022-04-26 22:38:09 +0530

    Strings

--- src/Strings/VideoQues/Comparison.java
+++ src/Strings/VideoQues/Comparison.java
@@ -3,12 +3,32 @@ package Strings.VideoQues;
 
 //to tell wether a and b are pointing to the same object or not
 
+import java.util.Arrays;
+
 public class Comparison {
     public static void main(String[] args) {
         String a="Aryan";
         String b="Aryan";
 
-        System.out.println(a==b);
+        int []arr= new int[56];
+
+        String name1=new String("Aryan");
+        String name2= new String("Aryan");
+
+        //== tellls if pointing to same object
+        System.out.println(name1==name2);
+
+        //dot equals tells if value of object is same
+        System.out.println(name1.equals(name2));
+
+
+        String c=a;
+       System.out.println(a==c);
+
+        System.out.println(Arrays.toString(new int[]{3, 4, 5, 6, 7}));
+
+        String name= null;
+        System.out.println(name);
 
     }
 }
---
+++ src/Strings/VideoQues/PrettyPrinting.java
@@ -0,0 +1,9 @@
+package Strings.VideoQues;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a=567.89344f;
+        //print format to get 3 decimal places
+        System.out.printf("Formatted no is %.3f", a);
+    }
+}
