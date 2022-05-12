a9519b0b2c4d2055db78df194382877905b467e3
Aryan Singh <aryansingh22070@gmail.com>
2022-05-12 21:30:30 +0530

    kadane algo

--- src/Strings/VideoQues/Immutability.java
+++ src/Strings/VideoQues/Immutability.java
@@ -1,12 +1,41 @@
 package Strings.VideoQues;
 
 public class Immutability {
-    public static void main(String[] args) {
-        String a="Aryan";
-        System.out.println(a);
+//    public static void main(String[] args) {
+//        String a="Aryan";
+//        System.out.println(a);
+//
+//        a="Singh";
+//        System.out.println(a);
+//
+//    }
 
-        a="Singh";
-        System.out.println(a);
-
-    }
+    //solved kadane's algo below
+//public static void main(String[] args) {
+//    int arr[]={-1,-2,-3,-4};
+//    int n=4;
+//
+//
+//
+//        int total=-10;
+//        for(int i=0; i<arr.length; i++){
+//            for(int j=i; j<arr.length; j++){
+//                if(total<sumofSubArray(arr, i, j)){
+//                    total = sumofSubArray(arr,  i,  j);
+//                }
+//            }
+//        }
+//
+//    System.out.println(total);
+//
+//}
+//
+//
+//    public static int sumofSubArray(int arr[], int start, int end){
+//        int sum=0;
+//        for(int i=start; i<=end; i++){
+//            sum+=arr[i];
+//        }
+//        return sum;
+//    }
 }
